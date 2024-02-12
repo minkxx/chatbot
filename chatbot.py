b7f525a991ba331c563f66027be16be9a7d4e958
minkxx <aryuokk@gmail.com>
2024-02-12 19:13:11 +0530

    add : colors in cmd input and output

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,16 @@
 # version 0.0.2
 
+from colorama import init
+
 import openai
 import json
 
 import config
+
+init(autoreset=True)
+YELLOW = "\x1b[1;33;40m"
+RED = "\x1b[1;31;40m"
+LIGHT_GREEN = "\033[1;32;40m"
 
 def fileLoadRead(file_path, role, content):
     with open(file_path, "r") as r:
@@ -31,7 +38,7 @@ class chatbot:
                 model="gpt-3.5-turbo",
                 messages=self.final_user_data,
                 temperature=1,
-                max_tokens=256,
+                max_tokens=128,
                 top_p=1,
                 frequency_penalty=0,
                 presence_penalty=0
@@ -47,10 +54,9 @@ if __name__ == "__main__":
 
     print("Chatroom - AnaAI")
     while True:
-        user_prompt = input("You : ")
+        user_prompt = input(f"{LIGHT_GREEN}You : ")
         if user_prompt == "q":
             break
         else:
             ai_prompt = Ana.chat(user_prompt)
-            print(f"AI : {ai_prompt}")
-
+            print(f"{YELLOW}AI : {ai_prompt}")
